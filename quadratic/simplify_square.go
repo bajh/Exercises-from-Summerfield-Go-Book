085b75374956a5e742045b4edbdeb41063c53d06
Brandon Hopkins <brhpkns@gmail.com>
2014-10-18 13:55:36 -0400

    Logic for simplifying radicals seems to be working, to be double checked later

--- quadratic/simplify_square.go
+++ quadratic/simplify_square.go
@@ -3,10 +3,49 @@ package main
 import (
   "fmt"
   "math"
+  "strconv"
 )
 
 func main() {
-  fmt.Println(primeFactorize(44))
+  fmt.Println(simplifySquareRoot(4500))
+}
+
+func simplifySquareRoot(number int) string {
+  if factors, prime, perfect := primeFactorize(number); prime {
+    return ("sqr" + strconv.Itoa(number))
+  } else if perfect {
+    return string(factors[0])
+  } else {
+    return simplify(factors)
+  }
+}
+
+func simplify(factors []int) string {
+  coefficient := 1
+  radicand := 1
+  fmt.Println(factors)
+  tallies := make(map[int]int)
+  for _, factor := range factors {
+    if _, found := tallies[factor]; found {
+      tallies[factor] += 1
+    } else {
+      tallies[factor] = 1
+    }
+  }
+  fmt.Println(tallies)
+  for factor, tally := range tallies {
+    if tally >= 2 {
+      coefficient *= factor * (tally / 2)
+      if tally % 2 != 0 {
+        radicand *= factor * (tally % 2)
+      }
+    } else {
+      radicand *= factor
+    }
+  }
+  fmt.Println(coefficient)
+  fmt.Println(radicand)
+  return "done"
 }
 
 func primeFactorize(number int) (factors []int, prime bool, perfect bool) {
@@ -19,9 +58,9 @@ func primeFactorize(number int) (factors []int, prime bool, perfect bool) {
     }
   }
   squareRootFloored := int(math.Sqrt(float64(number)))
-  if number / squareRootFloored == number {
+  if float64(number) / float64(squareRootFloored) == float64(squareRootFloored) {
     factors = append(factors, squareRootFloored, squareRootFloored)
-    return factors, false, true
+    return factors, true, false
   }
   for i := 2; i <= squareRootFloored; i++ {
     if number % i == 0 {
